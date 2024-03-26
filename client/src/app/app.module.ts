6f3c4383e010718adf81f8a351ee06d80f963fdc
Diana Mitkova <diana_guerdjilova@abv.bg>
2024-03-26 14:04:04 +0200

    Created Homepage, Login page, Register page, Profile page
    Generated the components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,10 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
+import { HomeComponent } from './components/pages/home/home/home.component';
+import { LoginComponent } from './components/pages/login/login/login.component';
+import { RegisterComponent } from './components/pages/register/register.component';
+import { ProfileComponent } from './components/pages/profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent,
   ],
   imports: [
     BrowserModule,
