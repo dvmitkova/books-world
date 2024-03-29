44035a5a3f75c4135ab2d4062f57323b45d965e6
Diana Mitkova <diana_guerdjilova@abv.bg>
2024-03-29 16:27:15 +0200

    Added Profile Component

---
+++ client/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: true,
+    firebaseConfig: {
+        apiKey: "AIzaSyCXMSHVrhfK4jWOMlhk5Q1hoLt4yT1cC1o",
+        authDomain: "books-world-e4977.firebaseapp.com",
+        databaseURL: "https://books-world-e4977-default-rtdb.europe-west1.firebasedatabase.app",
+        projectId: "books-world-e4977",
+        storageBucket: "books-world-e4977.appspot.com",
+        messagingSenderId: "821361731973",
+        appId: "1:821361731973:web:625c782386b17a14133cec",
+        measurementId: "G-HRPYYRG0HZ"
+    }
+}
