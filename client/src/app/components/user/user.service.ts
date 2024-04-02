2f7dba835ebcd453ebc5909137dbef8bc92219ea
Diana Mitkova <diana_guerdjilova@abv.bg>
2024-04-03 02:20:29 +0300

    Added Dynamic User Email Show in the Header

--- client/src/app/components/user/user.service.ts
+++ client/src/app/components/user/user.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, OnDestroy } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
@@ -8,17 +9,32 @@ import { ToastrService } from 'ngx-toastr';
 export class UserService {
   isLogggedIn = false;
 
-  constructor(private afs: AngularFireAuth, private toastr: ToastrService) {}
+  constructor(private afs: AngularFireAuth, private toastr: ToastrService, private router: Router) {}
 
   login(email: string, password: string) {
-    this.afs.signInWithEmailAndPassword(email, password).then(logRef => {
+    this.afs.signInWithEmailAndPassword(email, password).then((logRef) => {
       this.toastr.success('Logged in successfully');
+    }).catch(e => {
+      this.toastr.warning(e);
     })
+  }
+
+  loadUser() {
+    this.afs.authState.subscribe(user => {
+      localStorage.setItem('user', JSON.stringify(user));
+    });
   }
 
   register(user: { email: string; password: string }) {
     console.log(user);
 
     return this.afs.createUserWithEmailAndPassword(user.email, user.password);
+  }
+
+  logout() {
+    this.afs.signOut().then(() => {
+      this.toastr.success('User logged out successfully');
+      this.router.navigate(['/login'])
+    })
   }
 }
--- client/src/app/core/header/header.component.ts
+++ client/src/app/core/header/header.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/components/user/user.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  userEmail!: string;
+  constructor(private userService: UserService) {}
 
+  ngOnInit(): void {
+    const userData = localStorage.getItem('user');
+    if (userData) {
+      const user = JSON.parse(userData);
+      if (user && user.email) {
+        this.userEmail = user.email;
+      }
+    }
+  }
 }
