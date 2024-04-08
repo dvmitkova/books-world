3f85a8561cdfc092d2983ecd181d5c24f2d573a6
Diana Mitkova <diana_guerdjilova@abv.bg>
2024-04-08 10:14:11 +0300

    Fixed Auth Guard for Offer-Book Page and Profile

--- client/src/app/services/auth.guard.ts
+++ client/src/app/services/auth.guard.ts
@@ -1,38 +1,3 @@
-// import { Injectable } from '@angular/core';
-// import {
-//   ActivatedRouteSnapshot,
-//   CanActivate,
-//   Router,
-//   RouterStateSnapshot,
-//   UrlTree,
-// } from '@angular/router';
-// import { Observable } from 'rxjs';
-// import { UserService } from '../components/user/user.service';
-// import { ToastrService } from 'ngx-toastr';
-
-// @Injectable({ providedIn: 'root' })
-// export class AuthGuard implements CanActivate {
-//   constructor(private userService: UserService, private router: Router, private toastr: ToastrService) {}
-//   canActivate(
-//     route: ActivatedRouteSnapshot,
-//     state: RouterStateSnapshot
-//   ):
-//     | boolean
-//     | UrlTree
-//     | Observable<boolean | UrlTree>
-//     | Promise<boolean | UrlTree> {
-//     if (this.userService.isLoggedInGuard) {
-//       console.log('Access granted!');
-
-//       return true;
-//     } else {
-//       this.toastr.warning('You dont have permission to access this page!')
-//       this.router.navigate(['/auth/login'])
-//       return false;
-//     }
-//   }
-// }
-
 import { Injectable } from '@angular/core';
 import {
   ActivatedRouteSnapshot,
@@ -44,31 +9,28 @@ import {
 import { Observable } from 'rxjs';
 import { UserService } from '../components/user/user.service';
 import { ToastrService } from 'ngx-toastr';
-import { map } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanActivate {
   constructor(
     private userService: UserService,
     private router: Router,
-    private toastr: ToastrService
+    private toastr: ToastrService,
   ) {}
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
-  ): Observable<boolean | UrlTree> {
-    return this.userService.isLoggedIn().pipe(
-      map((loggedIn) => {
-        if (loggedIn) {
-          console.log('Access granted!');
-          return true;
-        } else {
-          this.toastr.warning('You do not have permission to access this page!');
-          // Redirect the user to the login page
-          return this.router.createUrlTree(['/auth/login']);
-        }
-      })
-    );
+  ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+
+    if (this.userService.isLoggedInGuard) {
+      console.log('Access granted!');
+      return true;
+    } else {
+      this.toastr.warning('Please login to access this page!');
+      this.router.navigate(['/auth/login']);
+      return false;
+    }
   }
 }
