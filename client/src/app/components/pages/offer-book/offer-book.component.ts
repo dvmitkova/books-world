6c60c8f919e9ca23c9583a1888b36c05df38f609
Diana Mitkova <diana_guerdjilova@abv.bg>
2024-04-08 10:22:54 +0300

    Revert "Revert "Fixed Auth Guard for Offer-Book Page and Profile""
    This reverts commit 47d176e89c8e8ac36501bba414189c5973f17149.

--- client/src/app/components/pages/offer-book/offer-book.component.ts
+++ client/src/app/components/pages/offer-book/offer-book.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-offer-book',
+//   templateUrl: './offer-book.component.html',
+//   styleUrls: ['./offer-book.component.css']
+// })
+// export class OfferBookComponent {
+
+// }
+
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../user/user.service';
 
 @Component({
   selector: 'app-offer-book',
   templateUrl: './offer-book.component.html',
   styleUrls: ['./offer-book.component.css']
 })
-export class OfferBookComponent {
+export class OfferBookComponent implements OnInit {
+  isLoggedIn: boolean = false;
 
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    // Subscribe to the isLoggedIn Observable from UserService
+    this.userService.isLoggedIn().subscribe((loggedIn) => {
+      this.isLoggedIn = loggedIn;
+    });
+  }
 }
