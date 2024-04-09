743143c0cd7fdb882483558e2265fc3e474d0770
Diana Mitkova <diana_guerdjilova@abv.bg>
2024-04-09 13:36:19 +0300

    Made Some Minor Design Changes

--- client/src/app/components/layouts/recently-added-books/recently-added-books.component.ts
+++ client/src/app/components/layouts/recently-added-books/recently-added-books.component.ts
@@ -6,17 +6,19 @@ import { Book } from 'src/app/types/book';
 @Component({
   selector: 'app-recently-added-books',
   templateUrl: './recently-added-books.component.html',
-  styleUrls: ['./recently-added-books.component.css']
+  styleUrls: ['./recently-added-books.component.css'],
 })
 export class RecentlyAddedBooksComponent implements OnInit {
-
   booksArray!: any[];
 
   constructor(private booksService: BooksService, private router: Router) {}
 
   ngOnInit(): void {
     this.booksService.loadData().subscribe((data: any[]) => {
-      this.booksArray = data.map(item => ({ id: item.id, data: item.data as Book }));
+      this.booksArray = data.map((item) => ({
+        id: item.id,
+        data: item.data as Book,
+      }));
     });
   }
 
