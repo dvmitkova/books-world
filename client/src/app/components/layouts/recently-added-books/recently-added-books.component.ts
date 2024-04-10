68728d756c7045eeed664840f678ee684efd5902
Diana Mitkova <diana_guerdjilova@abv.bg>
2024-04-10 13:19:14 +0300

    Finished the App
    Still have some bugs I don't have time to fix...

--- client/src/app/components/layouts/recently-added-books/recently-added-books.component.ts
+++ client/src/app/components/layouts/recently-added-books/recently-added-books.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { BooksService } from 'src/app/services/books.service';
 import { Book } from 'src/app/types/book';
 
@@ -11,27 +12,48 @@ import { Book } from 'src/app/types/book';
 export class RecentlyAddedBooksComponent implements OnInit {
   isLoading: boolean = true;
   booksArray!: any[];
+  isWishlistAdded: boolean = false;
+  isBookOrdered: boolean = false;
 
-  constructor(private booksService: BooksService, private router: Router) {}
+  constructor(
+    private booksService: BooksService,
+    private router: Router,
+    private toastr: ToastrService
+  ) {}
 
   ngOnInit(): void {
-    this.booksService.loadData().subscribe((data: any[]) => {
-      this.booksArray = data.map((item) => ({
-        id: item.id,
-        data: item.data as Book, 
-      }));
-      this.booksArray.sort((a, b) => b.data.added.toDate().getTime() - a.data.added.toDate().getTime());
-      this.isLoading = false;
-
-    },
-    error => {
-      console.error('Error loading data:', error);
-      this.isLoading = false; // Set isLoading to false if there's an error
-    }
+    this.booksService.loadData().subscribe(
+      (data: any[]) => {
+        this.booksArray = data.map((item) => ({
+          id: item.id,
+          data: item.data as Book,
+        }));
+        this.booksArray.sort(
+          (a, b) =>
+            b.data.added.toDate().getTime() - a.data.added.toDate().getTime()
+        );
+        this.isLoading = false;
+      },
+      (error) => {
+        console.error('Error loading data:', error);
+        this.isLoading = false; // Set isLoading to false if there's an error
+      }
     );
   }
 
   viewBookDetails(bookId: string) {
     this.router.navigate(['/book', bookId]);
+  }
+
+  addToWishlist(): void {
+    this.isWishlistAdded = true;
+    this.toastr.success('Added to your wishlist');
+
+  }
+
+  addToOrderedBooks() {
+    this.isWishlistAdded = true;
+    this.toastr.success('Added to your shopping bag');
+
   }
 }
